0b1fa3b06c044d46d193b2d0333b5bec1d0db5bb
JaviRSDEV <jarase2004@gmail.com>
2026-04-30 14:31:18 +0200

    minor changes

--- backend/src/main/java/com/carlog/backend/repository/UserJpaRepository.java
+++ backend/src/main/java/com/carlog/backend/repository/UserJpaRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserJpaRepository extends JpaRepository<User, Long> {
+public interface UserJpaRepository extends JpaRepository<User, String> {
     Optional<User> findByDni(String dni);
     Optional<User> findByName(String name);
     @Transactional
